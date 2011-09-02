5da979d7d5b3faecefa5072746700cd94400485b
Malini Das <mdas@mozilla.com>
2011-09-01 17:43:21 -0700

    Add logging and autolog submission code

--- profiler.py
+++ profiler.py
@@ -34,13 +34,16 @@
 #
 # ***** END LICENSE BLOCK *****
 
+import ConfigParser
+import logging
+from mozautolog import RESTfulAutologTestGroup
 from mozInstall import MozInstaller
-import urllib
 import os
 import re
 import stat
 import subprocess
 import shutil
+import urllib
 import zipfile
 
 PLAIN_REPEATS = 50
@@ -63,36 +66,48 @@ class ProfilerRunner(object):
         self.cert_path = None
         self.util_path = None
         self.app_name = None
+        self.log = None
+        self.hdlr = None
+        self.revision = None
 
     def start(self, builddata):
         assert(builddata['buildurl'])
         assert(builddata['testsurl'])
         assert(builddata['timestamp'])
         assert(builddata['platform'] == self.platform)
-
+        self.log = logging.getLogger("profiler_log_%s" % builddata['timestamp'])
+        self.hdlr = logging.FileHandler("profiler_log_%s" % builddata['timestamp'])
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        self.hdlr.setFormatter(formatter)
+        self.log.addHandler(self.hdlr) 
+        self.log.setLevel(logging.DEBUG)
+        
         # extract useful data from builddata
         self.builddata = builddata
-        self.temp_build_dir = 'profiler-%s' % builddata['timestamp']
-        self.plain_log_file = os.path.join(self.temp_build_dir, 'plainlog')
-        self.chrome_log_file = os.path.join(self.temp_build_dir, 'chromelog')
-        print "temp_build_dir: %s" % self.temp_build_dir
-
+        self.temp_build_dir = 'profiler_%s' % builddata['timestamp']
+        self.plain_log_file = os.path.join(self.temp_build_dir, 'plainlog_%s' % builddata['timestamp'])
+        self.chrome_log_file = os.path.join(self.temp_build_dir, 'chromelog_%s' % builddata['timestamp'])
         self.build_url = self.builddata['buildurl']
         self.build_file = os.path.join(self.temp_build_dir, self.build_url.rpartition("/")[2])
-        print "build_file: %s" % self.build_file
-
-
-        self.test_url = self.builddata['testsurl']
         self.test_path = os.path.join(self.temp_build_dir, 'tests')
         self.cert_path = os.path.join(self.test_path, 'certs')
         self.util_path = os.path.join(self.test_path, 'bin')
+        self.test_url = self.builddata['testsurl']
+
         if 'mac' in self.platform:
-            self.app_name = os.path.join(self.temp_build_dir, 'Minefield.app', 'Contents/MacOS/firefox-bin')
+            self.app_name = os.path.join(self.temp_build_dir, 'Minefield.app', 'Contents', 'MacOS', 'firefox-bin')
+            self.set_revision_from_ini(os.path.join(self.temp_build_dir, 'Minefield.app', 'Contents', 'MacOS', 'application.ini'))
         if 'linux' in self.platform:
             self.app_name = os.path.join(self.temp_build_dir, 'firefox', 'firefox-bin')
+            self.set_revision_from_ini(os.path.join(self.temp_build_dir, 'firefox', 'application.ini'))
         if 'win' in self.platform:
             self.app_name = os.path.join(self.temp_build_dir, 'firefox', 'firefox.exe')
-        print "test_path: %s" % self.test_path
+            self.set_revision_from_ini(os.path.join(self.temp_build_dir, 'firefox', 'application.ini'))
+
+        self.log.info("temp_build_dir: %s" % self.temp_build_dir)
+        self.log.info("buildurl: %s" % self.build_url)
+        self.log.info("testsurl: %s" % self.test_url)
+        self.log.info("revision id: %s" % self.revision)
 
         # make a temp directory for the build/tests to extract to and run in
         os.mkdir(self.temp_build_dir)
@@ -100,31 +115,35 @@ class ProfilerRunner(object):
             self.get_files()
             self.run_tests()
             self.parse_and_submit()
+            #delete logs only if we successfully finish everything
+            self.delete_log()
+        except Exception, e:
+            self.log.error(e)
         finally:
+            pass
             self.cleanup()
 
     def make_exec(self, path):
         def mod(arg, dir, names):
             for name in names:
-                print dir,name
                 os.chmod(os.path.join(dir, name), 0777)
         os.path.walk(path, mod, None)
 
     def get_files(self):
         # get the build
-        print "getting build_url"
+        self.log.info("Getting build_url")
         urllib.urlretrieve(self.build_url, self.build_file)
-        print "installing build"
+        self.log.info("Installing build")
         if 'mac' in self.platform:
-            MozInstaller(src=self.build_file, dest=self.temp_build_dir, dest_app='Minefield.app') # dest_app is only used for dmgs
+            MozInstaller(src=self.build_file, dest=self.temp_build_dir, dest_app='Minefield.app')
         else:
             MozInstaller(src=self.build_file, dest=self.temp_build_dir, dest_app='firefox')
 
         # get the tests.zip
-        print "getting tests.zip"
+        self.log.info("Getting tests.zip")
         test_zip = os.path.join(self.temp_build_dir, self.test_url.rpartition("/")[2])
         urllib.urlretrieve(self.test_url, test_zip)
-        print "unzipping, oh la la: %s" % test_zip
+        self.log.info("unzipping: %s" % test_zip)
         file = zipfile.ZipFile(test_zip, 'r')
         #file.external_attr = 0777 << 16L #TODO: doesn't work, but a solution like this is better than make_exec
         file.extractall(path=self.test_path)
@@ -134,16 +153,16 @@ class ProfilerRunner(object):
             self.make_exec(os.path.join(self.test_path, 'bin'))
             self.make_exec(os.path.join(self.test_path, 'mochitest'))
 
-    def cleanup(self):
-        print "cleaning"
-        shutil.rmtree(self.temp_build_dir)
+    def set_revision_from_ini(self, ini_file):
+        config = ConfigParser.RawConfigParser()
+        config.read(ini_file)
+        self.revision = config.get("App", "SourceStamp")
 
     def run_tests(self):
         # Unfortunate hack: we can't use --close-when-done on single tests, so just run all of Harness_sanity and pull out the relevant data
         # Can change this when Bug 508664 is resolved, as it will allow us to close single tests.
         runtests_location = os.path.join(self.test_path, 'mochitest', 'runtests.py')
         profile_location = os.path.join(self.test_path, 'mochitest', 'profile_path')
-        print "runtests_location: %s" % runtests_location
         try:
             shutil.rmtree(temp_dir_path)
         except:
@@ -167,7 +186,6 @@ class ProfilerRunner(object):
                                 ])
 
         for test_path in CHROME_TESTS:
-            print test_path
             ret = subprocess.call(['python',
                                     runtests_location,
                                     '--profile-path=%s' % profile_location,
@@ -181,7 +199,7 @@ class ProfilerRunner(object):
                                     '--close-when-done',
                                     '--autorun'
                                     ])
-        print 'Done running tests'
+        self.log.info("Done running tests")
 
     def parse_and_submit(self):
         plain_results = {}
@@ -200,7 +218,6 @@ class ProfilerRunner(object):
         logs.close()
         for k, v in plain_results.iteritems():
             plain_results[k] = plain_results[k] / PLAIN_REPEATS
-        print plain_results
         logs = open(self.chrome_log_file, 'r')
         for line in logs.readlines():
             matches = prof.findall(line)
@@ -212,15 +229,50 @@ class ProfilerRunner(object):
         logs.close()
         for k, v in chrome_results.iteritems():
             chrome_results[k] = chrome_results[k] / CHROME_REPEATS
-        print chrome_results
+        plain_results.update(chrome_results)
+        self.log.info("results: %s" % plain_results)
+
+        #submit
+        testgroup = RESTfulAutologTestGroup(
+          testgroup = 'mochitest-perf',
+          os = self.platform,
+          platform = self.platform,
+          builder = self.builddata['buildid'],
+          server = '10.2.76.100:9200',
+        )
+        testgroup.set_primary_product(
+          tree = self.builddata['tree'],
+          buildtype = self.builddata['buildtype'],
+          buildid = self.builddata['buildid'],
+          revision = self.revision,
+        )
+        for test, value in plain_results.iteritems():
+            testgroup.add_perf_data(
+              test = test,
+              type = 'ms',
+              time = value
+            )
+        self.log.info("Submitting to autolog")
+        #testgroup.submit()
+            
+
+    def delete_log(self):
+        self.log.removeHandler(self.hdlr)
+        self.hdlr.close()
+        os.remove("profiler_log_%s" % self.builddata['timestamp'])
         
-"""
+    def cleanup(self):
+        self.log.info("cleaning temp files")
+        shutil.rmtree(self.temp_build_dir)
+
 #for testing
 if __name__ == "__main__":
     pr = ProfilerRunner('macosx64')
     pr.start({'buildurl': 'ftp://ftp.mozilla.org/pub/firefox/tinderbox-builds/mozilla-central-macosx64/1314714989/firefox-9.0a1.en-US.mac.dmg',
               'testsurl': 'ftp://ftp.mozilla.org/pub/firefox/tinderbox-builds/mozilla-central-macosx64/1314714989/firefox-9.0a1.en-US.mac.tests.zip',
               'platform': 'macosx64',
+              'buildid': '12345',
+              'buildtype': 'opt',
+              'tree': 'mozilla-central',
               'timestamp': 100
               })
-"""
